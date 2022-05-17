d97075c8c9a3cedfedaae7d935b71cf7420ffd5e
Ye <yeeh1616@gmail.com>
2022-05-17 16:23:41 +0200

    1

--- module1/static/js/annotation.js
+++ module1/static/js/annotation.js
@@ -168,6 +168,31 @@ function save1(btn, qid, pid, column) {
     }
 }
 
+function save2(btn, qid, pid, column) {
+    var answerId = qid + "_answer";
+    var label = document.getElementById(qid + "_label");
+    var answer = document.getElementById(answerId).value;
+    var parmas = '{"pid":"' + pid + '","qid":"' + qid + '","answer":"' + answer + '","column":"' + column + '"}';
+
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var json = xhttp.responseText;
+            var obj = JSON.parse(json);
+
+            var complete = document.getElementById("complete");
+            complete.innerHTML = "Complete: " + obj["complete"] + "/" + obj["total"];
+
+            var q = document.getElementById("ap_" + qid);
+            q.className="box1";
+
+            btn.disabled=true;
+        }
+    };
+    xhttp.open("POST", "/policies/save2");
+    xhttp.send(parmas);
+}
+
 function save3(btn, qid, pid, column) {
     var answerName = qid + "_answer";
     var options = document.getElementById(answerName);
@@ -188,20 +213,4 @@ function save3(btn, qid, pid, column) {
             break;
         }
     }
-}
-
-function save2(btn, qid, pid, column) {
-    var answerId = qid + "_answer";
-    var label = document.getElementById(qid + "_label");
-    var answer = document.getElementById(answerId).value;
-    var parmas = '{"pid":"' + pid + '","qid":"' + qid + '","answer":"' + answer + '","column":"' + column + '"}';
-
-    const xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            btn.disabled=true;
-        }
-    };
-    xhttp.open("POST", "/policies/save2");
-    xhttp.send(parmas);
 }
