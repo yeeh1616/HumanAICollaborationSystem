b7d6bfc8f0578c22e4eb9e90c1f8f9f362e5ed38
Ye <yeeh1616@gmail.com>
2022-05-30 19:47:28 +0200

    1

--- module1/static/js/policies.js
+++ module1/static/js/policies.js
@@ -3,30 +3,10 @@ function search(){
     var url = '';
 
     if(pid == ""){
-        url = '/policies/searchAll';
+        url = '/policies/all';
     }else {
         url = '/policies/' + pid + '/search';
     }
 
     location.replace(url);
-}
-
-function handleChange(checkbox) {
-    let x = document.cookie
-  .split(';')
-  .reduce((res, c) => {
-    const [key, val] = c.trim().split('=').map(decodeURIComponent)
-    const allNumbers = str => /^\d+$/.test(str);
-    try {
-      return Object.assign(res, { [key]: allNumbers(val) ?  val : JSON.parse(val) })
-    } catch (e) {
-      return Object.assign(res, { [key]: val })
-    }
-  }, {});
-    let x2 = x['cb'];
-    if(checkbox.checked == true){
-        alert(''+checkbox.id+'checked');
-    }else{
-        alert(''+checkbox.id+'unchecked');
-   }
 }
